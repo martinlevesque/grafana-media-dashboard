8e4fe9d73d87c89571029ee51167a1088769593e
Martin Levesque <levesque.martin@gmail.com>
2023-10-12 22:54:01 -0400

    wip

--- disk_carbon_exporter.py
+++ disk_carbon_exporter.py
@@ -2,6 +2,7 @@
 import socket
 import time
 import os
+import re
 import argparse
 
 parser = argparse.ArgumentParser(
@@ -14,12 +15,32 @@ parser.add_argument("-m", "--mount-path")
 
 parsed_args = parser.parse_args()
 
-def df_json() -> dict:
+def df_json() -> list:
     cmd_line = "df"
     print(f"Running: {cmd_line}")
 
     result = os.popen(f"{cmd_line}").read()
-    print(f"result = {result}")
+    lines = result.splitlines()
+
+    return list(
+        filter(lambda x: x is not None,
+            list(map(lambda x: df_line_parse(x), lines))
+        )
+    )
+
+def df_line_parse(line):
+    words = re.split(r"\s+", line)
+
+    if words[0] == "Filesystem":
+        return None
+
+    perc_util = float(words[4].replace("%", "")) / 100.0
+    mountpoint = words[5]
+
+    return { "utilization": perc_util, "mount": mountpoint }
+
+
+
 
 df_json()
 
